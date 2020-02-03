19dbdfcf3a05fb7ec949a103cbc81c004189f5c2
isane-hash <isanejossy@gmail.com>
2020-02-03 07:49:47 +0200

    Updates

---
+++ precache-manifest.1c9f17c00a587a15af034e149deee936.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2047000138be2543a932c1e6a378efab",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "374ab6103d317e14d88b",
+    "url": "/ToDoList/static/css/main.99257b17.chunk.css"
+  },
+  {
+    "revision": "27130051255a819fa7ea",
+    "url": "/ToDoList/static/js/2.7bd08d42.chunk.js"
+  },
+  {
+    "revision": "374ab6103d317e14d88b",
+    "url": "/ToDoList/static/js/main.cc395360.chunk.js"
+  },
+  {
+    "revision": "6a71c43a298729cf07dd",
+    "url": "/ToDoList/static/js/runtime~main.67fadf76.js"
+  }
+]);
